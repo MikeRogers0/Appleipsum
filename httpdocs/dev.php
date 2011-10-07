0951a7649974e2b513c752ddcb5c008b49c52949
Mike Rogers <mike@fullondesign.co.uk>
2011-10-08 00:53:42 +0100

    Added copy and paste feature + prototyped up changes

---
+++ httpdocs/dev.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>Appleipsum</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="/css/uniform.default.css">
+
+  <script src="/js/libs/modernizr-2.0.6.min.js"></script>
+</head>
+
+<body>
+
+  <div id="container">
+    <header class="ipsum">
+    	<h1>iPsum</h1>
+    	<h2>Introducing Apple iPsum - It's the moust amazing placeholder text yet.</h2>
+    </header>
+    <form id="main" role="main">
+    	<section id="switcher"><img src="img/placeholders/how_much_genius_do_you_want.png" alt="how_much_genius_do_you_want" width="462" height="34" /> 
+    	<div id="lipsum_levels">
+	    	<select name="lipsum_levels">
+	    		<option value="0">1 Paragraph</option>
+	    		<option value="1" selected>2 Paragraphs</option>
+	    		<option value="2">3 Paragraphs</option>
+	    	</select>
+    	</div>
+    	</section>
+    	<section id="controls">
+    		<a href="#text" data-type="text" id="copy_text">Copy text to clipboard</a>
+    		<a href="#html" data-type="html" id="copy_html">Copy HTML to clipboard</a>
+    	</section>
+    	<section id="lipsum_code" contenteditable="true"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies dui in eros imperdiet faucibus a et tellus. Praesent elit eros, ornare vel vestibulum id, tristique in orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed tempor consectetur tortor, sed pharetra est aliquam quis. Donec sem sapien, varius at pharetra vel, adipiscing non turpis. Duis elementum, ipsum eu porta laoreet, nisi ipsum tempus risus, eu varius augue nisl in libero. Duis eget nulla id augue consectetur congue. Quisque lacinia tortor eu libero fermentum viverra. Morbi risus justo, dapibus ac sollicitudin eget, interdum blandit risus.</p>
+
+<p>Sed aliquet velit sed nunc mollis vitae hendrerit ipsum eleifend. Quisque convallis nisi quis lorem elementum condimentum. Donec at nunc massa, nec tincidunt dui. Donec suscipit rutrum nulla sit amet ornare. Integer faucibus ultricies elit sit amet tristique. Mauris est turpis, viverra vel lobortis eget, bibendum sed ipsum. Donec iaculis blandit libero, vitae feugiat neque cursus vel. Cras ut magna id purus fermentum imperdiet. Nunc malesuada, turpis non iaculis iaculis, sem urna accumsan turpis, ut placerat nibh neque sed nulla. Proin quam ante, iaculis tristique laoreet et, aliquet ut nibh. Quisque ultricies sollicitudin nibh sit amet pellentesque.</p></section>
+    	
+    	<section id="TEMPcontrols">
+    		<a href="" data-theme="ipsum">ipsum</a>
+    		<a href="" data-theme="macipsum">macipsum</a>
+    	</section>
+    </form>
+    <footer>
+    	<img src="img/placeholders/footer.png" alt="footer" width="855" height="117" />
+    </footer>
+  </div> <!--! end of #container -->
+  
+  <div id="full_lipsum" class="hidden">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia lectus non orci lacinia commodo. Mauris volutpat egestas consectetur. In hac habitasse platea dictumst. Ut adipiscing mattis suscipit. Etiam facilisis, mauris quis mattis aliquet, neque est ultricies ipsum, eget condimentum lorem elit in augue. In at tincidunt erat. Nulla in enim nisi. Nunc ac orci lorem. Sed tempus, sapien sed lacinia fringilla, turpis erat blandit lorem, eget volutpat augue est non lectus. Pellentesque porta fringilla libero vel scelerisque. Integer sed erat a sapien auctor ultricies id eget magna. Proin quis lacus at eros facilisis porta eu venenatis ligula. Mauris sed ipsum eget ipsum ullamcorper lacinia.</p>
+<p>Cras sapien mi, posuere vitae ornare non, gravida id lectus. Etiam pulvinar justo nec dolor blandit non facilisis velit pharetra. Suspendisse eget libero velit, sit amet rhoncus mauris. Quisque vitae metus nec lacus tempor aliquet. Donec nec massa euismod elit faucibus dignissim sit amet sit amet est. Integer eros ligula, scelerisque quis lobortis sit amet, mattis et quam. Vivamus suscipit feugiat massa sit amet accumsan. Praesent vel quam et nulla semper tristique et eget felis.</p>
+<p>Vivamus non ante velit. Sed elementum enim non lorem consectetur et elementum arcu commodo. Mauris accumsan malesuada sagittis. Nullam sit amet turpis et tortor blandit gravida id ut felis. Integer auctor suscipit leo aliquam tincidunt. Donec vel risus at tortor posuere fringilla. Quisque sed tellus sit amet velit faucibus sodales. Maecenas cursus turpis vel magna rutrum placerat ut vitae velit. Suspendisse sed mauris augue, vel tincidunt nisi. Integer blandit felis quis ipsum ultricies at volutpat nunc elementum. Nulla consequat velit sed velit iaculis accumsan. Curabitur vestibulum, felis eu vulputate molestie, turpis sapien porttitor tellus, non sagittis neque nisl ac nisl.</p>
+<p>Aliquam erat volutpat. Nulla ut nunc urna, quis fermentum risus. Praesent sagittis odio et tortor consequat malesuada. Vestibulum eget enim massa, eu dictum tellus. Proin mollis euismod sapien sit amet venenatis. Pellentesque rhoncus purus ac lacus vestibulum ut mollis arcu porta. Curabitur iaculis ornare bibendum. Etiam ac turpis tellus, sit amet accumsan turpis. Curabitur scelerisque vulputate dictum. Quisque quis leo ut magna volutpat pharetra.</p>
+  </div>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+  
+  <script src="/js/libs/ZeroClipboard.js"></script>
+  <script defer src="/js/libs/jquery.uniform.min.js"></script>
+  <script defer src="/js/script.js"></script>
+
+  <!--[if lt IE 7 ]>
+    <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+    <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+  <![endif]-->
+  
+</body>
+</html>
--- httpdocs/js/script.js
+++ httpdocs/js/script.js
@@ -1,26 +1,57 @@
-/* Author: 
-
+/* Author: Mike Rogers @Rogem002
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(function(){
+	// Set up page and declare variable were going to use often.
+	$('select').uniform();
+	var lipsum_levels = $('#lipsum_levels select');
+	var lipsum_code = $('#lipsum_code');
+	var full_lipsum = $('#full_lipsum p');
+	var controls = $('#controls a');
+	var theme_changer = $('#TEMPcontrols a');
+	
+	// The clipboard stuff is a pain, so I used http://code.google.com/p/zeroclipboard/
+	var clipboard_data = null;
+	
+	controls.each(function(){
+		var clip = new ZeroClipboard.Client();
+		clip.setHandCursor( true );
+		clip.glue($(this).attr('id'));
+		if($(this).attr('data-type') == 'html'){
+			clip.setText( lipsum_code.html());
+		}else{
+			clip.setText( lipsum_code.text());
+		}
+		
+		
+		clip.addEventListener('complete', function (client, text) {
+		alert('copied');
+			$(this).trigger('click');
+		});
+	});
+	
+	
+	
+	// Add functions for page.
+	function update_lipsum(){
+		lipsum_code.animate({height: (parseInt(lipsum_levels.val()) + 1) * 122+'px'});
+		lipsum_code.html(''); // Clear the old lipsum text.
+		for(var i=0;i<=lipsum_levels.val();i++){
+			lipsum_code.append(full_lipsum[i]);
+		}
+	}
+	
+	function change_theme(){
+		var new_theme = $(this).attr('data-theme');
+		$('h1, h2').fadeOut(400, function(){
+			$('header').attr('class', new_theme);
+			$('h1, h2').fadeIn(700);
+		});
+		return false;
+	}
+	
+	
+	// Add listners
+	lipsum_levels.bind('change', update_lipsum);
+	theme_changer.bind('click', change_theme);
+});
